b35598303ff9af94d4e299da74b4cca384f9ecb0
Mohamed Zaitoon (التوني) <125979629+mohamed-zaitoon@users.noreply.github.com>
2024-03-21 14:07:56 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -137,7 +137,7 @@ Since: 06/08/2016
 		};
 	  
 	  $scope.goToTikTok = function(){
-			document.location.assign("https://tiktok.com/@mohamedzaitoon.tk");
+			document.location.assign("https://tiktok.com/@mohamedzaitoon_tk");
 		};
 	  
  $scope.goToTwitter = function(){
